20fa34532010119006131d036bfc7ae36d1cf866
chanhl22 <chanhlee@student.42seoul.kr>
2021-04-08 12:56:26 +0900

    17425

--- Codeplus/src/Test.java
+++ Codeplus/src/Test.java
@@ -3,7 +3,12 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println(123);
+		int[] arr = new int[6];
+		for (int i = 1; i <= 5; i++) {
+			arr[i] = i+1;
+		}
+		for (int j = 0; j <= 5; j++) {
+			System.out.println(arr[j]);			
+		}
 	}
-
 }
