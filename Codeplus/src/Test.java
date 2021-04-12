cedc06fd5bfbc56cd3d9dcdc6ec66c84dd7550c8
chanhl22 <chanhlee@student.42seoul.kr>
2021-04-12 12:35:23 +0900

    2309

--- Codeplus/src/Test.java
+++ Codeplus/src/Test.java
@@ -4,11 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 
 		int[] arr = new int[6];
-		for (int i = 1; i <= 5; i++) {
-			arr[i] = i+1;
-		}
-		for (int j = 0; j <= 5; j++) {
-			System.out.println(arr[j]);			
-		}
+		System.out.println(arr.length);
 	}
 }
