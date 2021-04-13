77a60648582047680566263a28c650444dfc03b5
chanhl22 <chanhlee@student.42seoul.kr>
2021-04-13 14:33:15 +0900

    6064

--- Codeplus/src/Test.java
+++ Codeplus/src/Test.java
@@ -4,10 +4,19 @@ public class Test {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = 0;
-		int res = 0;
-		
-		res = a/10;
-		System.out.println(res);
+		int num = sc.nextInt();
+		while (num-- > 0) {
+			String[] s = sc.nextLine().split(" ");
+			
+			int M = Integer.parseInt(s[0]);
+			int N = Integer.parseInt(s[1]);
+			int x = Integer.parseInt(s[2]) - 1;
+			int y = Integer.parseInt(s[3]) - 1;
+			
+			System.out.println(s[0]);
+			System.out.println(s[1]);
+			System.out.println(s[2]);
+			System.out.println(s[3]);
+		}
 	}
 }
