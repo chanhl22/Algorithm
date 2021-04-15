19c78f6fa554bdf68e47b56c99ca303b6335d7a7
chanhl22 <chanhlee@student.42seoul.kr>
2021-04-15 13:50:18 +0900

    18290

--- Codeplus/src/Test.java
+++ Codeplus/src/Test.java
@@ -1,22 +1,23 @@
 import java.util.*;
 
 public class Test {
-
+	static int[] dx = {0,0,1,-1};
+	static int[] dy = {1,-1,0,0};
+	static boolean[][] c = new boolean[10][10];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		while (num-- > 0) {
-			String[] s = sc.nextLine().split(" ");
-			
-			int M = Integer.parseInt(s[0]);
-			int N = Integer.parseInt(s[1]);
-			int x = Integer.parseInt(s[2]) - 1;
-			int y = Integer.parseInt(s[3]) - 1;
-			
-			System.out.println(s[0]);
-			System.out.println(s[1]);
-			System.out.println(s[2]);
-			System.out.println(s[3]);
+		int n = 2;
+		int m = 2;
+		boolean ok = true;
+		for (int i=0; i<4; i++) {
+			 int nx = dx[i];
+			 int ny = dy[i];
+			 if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+				 ok = false;
+				 System.out.println("asdfds");
+			 }
+			 System.out.println(ok);
 		}
+		System.out.println(ok);
 	}
 }
