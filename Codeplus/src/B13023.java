026f4b5b1b795c8c4bf01ada59707030a649ed40
chanhl22 <chanhlee@student.42seoul.kr>
2021-05-09 10:43:25 +0900

    13023

--- Codeplus/src/B13023.java
+++ Codeplus/src/B13023.java
@@ -1,19 +1,56 @@
 import java.util.*;
 
+class Edge {
+	int from, to;
+	Edge(int from, int to) {
+		this.from = from;
+		this.to = to;
+	}
+}
+
 public class B13023 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		boolean[][] a = new boolean[n][n];
-		ArrayList<Integer>[] g = (ArrayList<Integer>[]) new ArrayList[n];
+		boolean[][] a = new boolean[n][n];  //인접행렬
+		ArrayList<Integer>[] g = new ArrayList[n];  //인접리스트
+		ArrayList<Edge> edges = new ArrayList<Edge>();  //간접리스트
+		for (int i = 0; i < n; i++) {
+			g[i] = new ArrayList<Integer>();
+		}
+		//초기화
 		for (int i = 0; i < m; i++) {
 			int from = sc.nextInt();
 			int to = sc.nextInt();
-			
+			edges.add(new Edge(from, to));
+			edges.add(new Edge(to, from));
+			a[from][to] = true;
+			a[to][from] = true;
+			g[from].add(to);
+			g[to].add(from);
 		}
-
+		m *= 2;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < m; j++) {
+				int A = edges.get(i).from;
+				int B = edges.get(i).to;
+				int C = edges.get(j).from;
+				int D = edges.get(j).to;
+				if (A == B || A == C || A == D || B == C || B == D || C == D)
+					continue;
+				if (!a[B][C])
+					continue;
+				for (int E : g[D]) {
+					if (A == E || B == E || C == E || D == E)
+						continue;
+					System.out.println(1);
+					System.exit(0);
+				}
+			}
+		}
+		System.out.println(0);
 	}
 
 }
