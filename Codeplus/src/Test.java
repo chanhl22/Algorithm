4a115af2f8e382cf5f121f56c71bcacf62ebfccf
chanhl22 <chanhlee@student.42seoul.kr>
2021-05-17 22:13:47 +0900

    14499

--- Codeplus/src/Test.java
+++ Codeplus/src/Test.java
@@ -41,21 +41,27 @@ class Person{
 
 public class Test {
 	public static void main(String[] args) {
-		Person a = new Person("Noah");
-		Person b = new Person("Noah");
-	
-		System.out.println(a == b);
-	
-		String a2 = "noah";
-		String b2 = "noah";
-		System.out.println(a2 == b2);
-		
-		String a3 = new String("Noah");
-		String b3 = new String("Noah");
-		System.out.println(a3 == b3);
-		HashSet<String> a1 = new HashSet<String>();
-		a1.add(a2);
-		a1.add(b2);
-		System.out.println(a1.size());
+//		Person a = new Person("Noah");
+//		Person b = new Person("Noah");
+//	
+//		System.out.println(a == b);
+//	
+//		String a2 = "noah";
+//		String b2 = "noah";
+//		System.out.println(a2 == b2);
+//		
+//		String a3 = new String("Noah");
+//		String b3 = new String("Noah");
+//		System.out.println(a3 == b3);
+//		HashSet<String> a1 = new HashSet<String>();
+//		a1.add(a2);
+//		a1.add(b2);
+//		System.out.println(a1.size());
+		ArrayList<String[]> list2 = new ArrayList<String[]>();  //String[] 타입을 받는 ArrayList 생성
+		list2.add(new String[]{"가을", "하늘"});                 //요소를 추가한다.
+
+		for(int t=0; t<list2.size(); t++)                            //요소의 크기만큼 돌면서
+		   System.out.println(list2.get(t)[0]);    
+
 	}
 }
