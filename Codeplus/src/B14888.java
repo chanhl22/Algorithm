ccae954da5f277e32e20cb5d91b38699da9022b7
chanhl22 <chanhlee@student.42seoul.kr>
2021-05-25 14:59:15 +0900

    14888

---
+++ Codeplus/src/B14888.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class B14888 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int[] op = new int[4];
+        for (int i = 0; i < 4; i++) {
+            op[i] = sc.nextInt();
+        }
+    }
+}
