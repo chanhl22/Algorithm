58c3b8b00cc52d82663fea3ad0bf345e91f3aba8
chanhl22 <chanhlee@student.42seoul.kr>
2021-06-04 20:33:48 +0900

    16929

--- Codeplus/src/B2580.java
+++ Codeplus/src/B2580.java
@@ -1,9 +1,31 @@
+import java.util.*;
 
 public class B2580 {
-
+	static int x = 0;
+	static int y = 0;
+	
+	static void go(int z) {
+		if (z == 81) {
+			return ;
+		}
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
+				go(9 * x + y);				
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int[][] a = new int[9][9];
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+		
+		go (0);
+		
 	}
 
 }
