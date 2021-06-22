a0e82d8dd28eb475e3c0177c21e352e987f178c4
chanhl22 <chanhlee@student.42seoul.kr>
2021-06-22 23:04:19 +0900

    17822

--- Codeplus/src/B17822.java
+++ Codeplus/src/B17822.java
@@ -1,8 +1,38 @@
+import java.util.*;
 
 public class B17822 {
+	static void go(int [][] a, int x, int d, int k) {
+		List<int[]> test = new ArrayList<>();
+		if (d == 0) {
+			test.addAll(Arrays.asList(a[x]));
+		} else {
+			test.addAll(Arrays.asList(a[x]));
+		}
+		for(int[] t : test) {
+			System.out.println(test);
+		}
+		//System.out.println(test);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int t = sc.nextInt();
+		int	[][] a = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+		while (t-- > 0) {
+			int x = sc.nextInt();
+			int d = sc.nextInt();
+			int k = sc.nextInt();
+			for (int i = x; i < n; i += x) {
+				go(a, i, d, k);
+			}
+		}
 
 	}
 
