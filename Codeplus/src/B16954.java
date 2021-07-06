620754e59caf6fe27dc0db60c8f679eab748a3b5
chanhl22 <chanhlee@student.42seoul.kr>
2021-07-06 22:08:11 +0900

    Another way to solve

--- Codeplus/src/B16954.java
+++ Codeplus/src/B16954.java
@@ -27,15 +27,15 @@ public class B16954 {
             int x = q.remove();
             int y = q.remove();
             int t = q.remove();
-            if(x == 0 && y == 7) ans = true;
+            if (x == 0 && y == 7) ans = true;
             for (int k = 0; k < 9; k++) {
                 int nx = x + dx[k];
                 int ny = y + dy[k];
-                int nt = Math.min(t+1,8);
+                int nt = Math.min(t + 1, 8);
                 if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
-                    if (nx-t >= 0 && a[nx-t][ny] == '#') continue;
-                    if (nx-t-1 >= 0 && a[nx-t-1][ny] == '#') continue;
-                    if(check[nx][ny][nt] == false){
+                    if (nx - t >= 0 && a[nx - t][ny] == '#') continue;
+                    if (nx - t - 1 >= 0 && a[nx - t - 1][ny] == '#') continue;
+                    if (check[nx][ny][nt] == false) {
                         check[nx][ny][nt] = true;
                         q.add(nx);
                         q.add(ny);
@@ -47,3 +47,45 @@ public class B16954 {
         System.out.println(ans ? 1 : 0);
     }
 }
+
+//another way to solve
+/*
+import java.util.*;
+public class B16954 {
+    final static int[] dx = {0,0,1,-1,1,-1,1,-1,0};
+    final static int[] dy = {1,-1,0,0,1,1,-1,-1,0};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 8;
+        String[] a = new String[n];
+        for (int i=0; i<n; i++) {
+            a[i] = sc.next();
+        }
+        Queue<Integer> q = new LinkedList<>();
+        boolean[][][] check = new boolean[8][8][9];
+        check[7][0][0] = true;
+        q.add(7); q.add(0); q.add(0);
+        boolean ans = false;
+        while (!q.isEmpty()) {
+            int x = q.remove();
+            int y = q.remove();
+            int t = q.remove();
+            if (x == 0 && y == 7) ans = true;
+            for (int k=0; k<9; k++) {
+                int nx = x+dx[k];
+                int ny = y+dy[k];
+                int nt = Math.min(t+1, 8);
+                if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                    if (nx-t >= 0 && a[nx-t].charAt(ny) == '#') continue;
+                    if (nx-t-1 >= 0 && a[nx-t-1].charAt(ny) == '#') continue;
+                    if (check[nx][ny][nt] == false) {
+                        check[nx][ny][nt] = true;
+                        q.add(nx); q.add(ny); q.add(nt);
+                    }
+                }
+            }
+        }
+        System.out.println(ans ? 1 : 0);
+    }
+}
+*/
