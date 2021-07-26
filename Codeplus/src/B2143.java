d09ecfe15337b75ef99ae5239a99033b09fb0fa8
chanhl22 <chanhlee@student.42seoul.kr>
2021-07-26 20:36:59 +0900

    11048_1

---
+++ Codeplus/src/B2143.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class B2143 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = sc.nextInt();
+        }
+
+
+    }
+}
