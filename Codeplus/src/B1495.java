aa8b23cb4a091f174a71181ec0002bf6fbbaea4a
chanhl22 <chanhlee@student.42seoul.kr>
2021-07-30 10:46:28 +0900

    Add another solution

--- Codeplus/src/B1495.java
+++ Codeplus/src/B1495.java
@@ -32,3 +32,28 @@ public class B1495 {
         System.out.println(ans);
     }
 }
+
+//Another solution
+//import java.util.*;
+//public class Main {
+//    public static void main(String args[]) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int k = sc.nextInt();
+//        int[] w = new int[n+1];
+//        int[] v = new int[n+1];
+//        for (int i=1; i<=n; i++) {
+//            w[i] = sc.nextInt();
+//            v[i] = sc.nextInt();
+//        }
+//        int[] d = new int[k+1];
+//        for (int i=1; i<=n; i++) {
+//            for (int j=k; j>=1; j--) {
+//                if (j-w[i] >= 0) {
+//                    d[j] = Math.max(d[j], d[j-w[i]]+v[i]);
+//                }
+//            }
+//        }
+//        System.out.println(d[k]);
+//    }
+//}
