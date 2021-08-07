d0d036238803ceb7a6b6d04cc1708960d28071ca
chanhl22 <chanhlee@student.42seoul.kr>
2021-08-07 14:07:02 +0900

    17406

--- Codeplus/src/B17406.java
+++ Codeplus/src/B17406.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
-class Tuple1 implements Comparable<Tuple1>{
+class Tuple1 implements Comparable<Tuple1> {
     int r;
     int c;
     int s;
@@ -17,7 +20,7 @@ class Tuple1 implements Comparable<Tuple1>{
     public int compareTo(Tuple1 that) {
         if (this.index > that.index) {
             return 1;
-        } else if (this.index == that.index){
+        } else if (this.index == that.index) {
             return 0;
         } else {
             return -1;
@@ -27,21 +30,21 @@ class Tuple1 implements Comparable<Tuple1>{
 
 public class B17406 {
     static boolean next_permutation(Tuple1[] a) {
-        int i = a.length-1;
-        while (i > 0 && a[i-1].compareTo(a[i]) >= 0) {
+        int i = a.length - 1;
+        while (i > 0 && a[i - 1].compareTo(a[i]) >= 0) {
             i -= 1;
         }
         if (i <= 0) {
             return false;
         }
-        int j = a.length-1;
-        while (a[j].compareTo(a[i-1]) <= 0) {
+        int j = a.length - 1;
+        while (a[j].compareTo(a[i - 1]) <= 0) {
             j -= 1;
         }
-        Tuple1 temp = a[i-1];
-        a[i-1] = a[j];
+        Tuple1 temp = a[i - 1];
+        a[i - 1] = a[j];
         a[j] = temp;
-        j = a.length-1;
+        j = a.length - 1;
         while (i < j) {
             temp = a[i];
             a[i] = a[j];
@@ -50,6 +53,60 @@ public class B17406 {
             j -= 1;
         }
         return true;
+    }
+
+    static void go(int[][] a, Tuple1 t) {
+        int row = t.r;
+        int col = t.c;
+        int size = t.s;
+        //일차원 배열에 넣기
+        ArrayList<ArrayList<Integer>> groups = new ArrayList<>();
+        for (int s = 1; s <= size; s++) {
+            ArrayList<Integer> group = new ArrayList<>();
+            //상
+            for (int r = row - s, c = col - s; c < col + s; c++) {
+                group.add(a[r][c]);
+            }
+            //우
+            for (int r = row - s, c = col + s; r < row + s; r++) {
+                group.add(a[r][c]);
+            }
+            //하
+            for (int r = row + s, c = col + s; c > col - s; c--) {
+                group.add(a[r][c]);
+            }
+            //좌
+            for (int r = row + s, c = col - s; r > row - s; r--) {
+                group.add(a[r][c]);
+            }
+            groups.add(group);
+        }
+        //일차원 배열에 넣었으면 회전
+        for (int s = 1; s <= size; s++) {
+            ArrayList<Integer> group = groups.get(s-1);
+            Collections.rotate(group, 1);
+            int index = 0;
+            //상
+            for (int r = row - s, c = col - s; c < col + s; c++) {
+                a[r][c] = group.get(index);
+                index++;
+            }
+            //우
+            for (int r = row - s, c = col + s; r < row + s; r++) {
+                a[r][c] = group.get(index);
+                index++;
+            }
+            //하
+            for (int r = row + s, c = col + s; c > col - s; c--) {
+                a[r][c] = group.get(index);
+                index++;
+            }
+            //좌
+            for (int r = row + s, c = col - s; r > row - s; r--) {
+                a[r][c] = group.get(index);
+                index++;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -65,15 +122,32 @@ public class B17406 {
         }
         Tuple1[] d = new Tuple1[k];
         for (int i = 0; i < k; i++) {
-            int r = sc.nextInt()-1;
-            int c = sc.nextInt()-1;
+            int r = sc.nextInt() - 1;
+            int c = sc.nextInt() - 1;
             int s = sc.nextInt();
             d[i] = new Tuple1(r, c, s, i);
         }
         int ans = Integer.MAX_VALUE;
         do {
-
-
-        }while(next_permutation(d));
+            int[][] b = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    b[i][j] = a[i][j];
+                }
+            }
+            for (Tuple1 t : d) {
+                go(b, t);
+            }
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 0; j < m; j++) {
+                    sum += b[i][j];
+                }
+                if (ans > sum) {
+                    ans = sum;
+                }
+            }
+        } while (next_permutation(d));
+        System.out.println(ans);
     }
 }
