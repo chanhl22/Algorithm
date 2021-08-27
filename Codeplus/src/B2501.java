d2b8f335c27b576d5d58a6c3c9f76fac608f8476
chanhl22 <chanhlee@student.42seoul.kr>
2021-08-27 14:45:12 +0900

    Add Another Solution

--- Codeplus/src/B2501.java
+++ Codeplus/src/B2501.java
@@ -19,3 +19,28 @@ public class B2501 {
         }
     }
 }
+
+//Another Solution
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    static int N,K;
+//    static Scanner sc = new Scanner(System.in);
+//
+//    public static void main(String[] args)  {
+//        N = sc.nextInt();
+//        K = sc.nextInt();
+//
+//        for (int i = 1; i <= N; i++) {
+//            if(N % i == 0) K--;
+//
+//            if(K == 0) {
+//                System.out.println(i);
+//                break;
+//            }
+//        }
+//        if(K != 0)
+//            System.out.println(0);
+//    }
+//}
