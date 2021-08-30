60916b752a81dce999f3db4a05d51343ada3eaed
chanhl22 <chanhlee@student.42seoul.kr>
2021-08-30 19:52:14 +0900

    Add Solution 11170

--- Codeplus/src/B11170.java
+++ Codeplus/src/B11170.java
@@ -30,3 +30,36 @@ public class B11170 {
         }
     }
 }
+
+//Another Solution
+//import java.util.Scanner;
+//
+//public class B11170 {
+//    static int calc(int num) {
+//        int ret = 0;
+//        for (char c : String.valueOf(num).toCharArray()) {
+//            if (c == '0'){
+//                ret++;
+//            }
+//        }
+//        return ret;
+//    }
+//
+//    static int solve(int n, int m) {
+//        int ret = 0;
+//        for (int i = n; i <= m; i++) {
+//            ret += calc(i);
+//        }
+//        return ret;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        while (t-- > 0) {
+//            int n = sc.nextInt();
+//            int m = sc.nextInt();
+//            System.out.println(solve(n,m));
+//        }
+//    }
+//}
