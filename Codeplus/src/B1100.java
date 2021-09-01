e1668a61267cc2a6b68c5d25a0e926570a8fb766
chanhl22 <chanhlee@student.42seoul.kr>
2021-09-01 13:23:01 +0900

    1100

--- Codeplus/src/B1100.java
+++ Codeplus/src/B1100.java
@@ -31,3 +31,32 @@ public class B1100 {
         System.out.println(ans);
     }
 }
+
+//Another Solution
+//import java.util.Scanner;
+//
+//public class WhiteSpace_Sol_2 {
+//  public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//    final int SIZE = 8;
+//    // 체스판
+//    char[][] chessBoard = new char[SIZE][SIZE];
+//    // 하얀 칸 위의 말 개수
+//    int num = 0;
+//
+//    String str = "";
+//    for (int i = 0; i < SIZE; i++) {
+//      str = scanner.nextLine();
+//      for (int j = 0; j < SIZE; j++) {
+//        chessBoard[i][j] = str.charAt(j);
+//
+//        // (i + j)를 2로 나눈 나머지가 0인 경우 => 하얀 칸이다.
+//        if ((i + j) % 2 == 0 && chessBoard[i][j] == 'F') {
+//            num++;
+//        }
+//      }
+//    }
+//
+//    System.out.println(num);
+//  }
+//}
